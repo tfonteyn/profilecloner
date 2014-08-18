b87980b65e092ef2e31ea0090373cd2ef1796703
Tom Fonteyne <tom.fonteyne@redhat.com>
2014-08-18 11:35:48 +0100

    removed version, so rename the jar first

--- profilecloner.sh
+++ profilecloner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CLONER=./target/profilecloner-2014-08-14.jar
+CLONER=$(dirname "$0")/profilecloner.jar
 
 java -cp $JBOSS_HOME/bin/client/jboss-cli-client.jar:$CLONER org.jboss.tfonteyne.profilecloner.Main @$
 
