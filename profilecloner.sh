a0bc13c57a88eee8200f6f49c4db31d2312055e7
Tom Fonteyne <tom.fonteyne@redhat.com>
2014-08-21 16:14:41 +0100

    Typo

--- profilecloner.sh
+++ profilecloner.sh
@@ -2,5 +2,5 @@
 
 CLONER=$(dirname "$0")/profilecloner.jar
 
-java -cp $JBOSS_HOME/bin/client/jboss-cli-client.jar:$CLONER org.jboss.tfonteyne.profilecloner.Main @$
+java -cp $JBOSS_HOME/bin/client/jboss-cli-client.jar:$CLONER org.jboss.tfonteyne.profilecloner.Main "$@"
 
